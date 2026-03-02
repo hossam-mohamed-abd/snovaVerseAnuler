963f4080b6e9f503a3332f020c132a231b76aec9
Hossam Mohamed <hossam545mohamed@gmail.com>
2026-03-02 22:11:46 +0200

    add TourismVirtualTour

---
+++ src/app/components/ar-view/ar-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ar-view',
+  imports: [],
+  templateUrl: './ar-view.component.html',
+  styleUrl: './ar-view.component.css',
+})
+export class ArViewComponent {
+
+}
--- src/app/components/home/children/projects-section/projects-section.component.ts
+++ src/app/components/home/children/projects-section/projects-section.component.ts
@@ -1,1 +1,15 @@
-import { Component } from '@angular/core'; import { LanguageService } from '../../../../core/language'; @Component({ selector: 'app-projects-section', imports: [], templateUrl: './projects-section.component.html', styleUrl: './projects-section.component.css', }) export class ProjectsSectionComponent { constructor(public langService: LanguageService) {} get currentLang() { return this.langService.getLang(); } }
+import { Component } from '@angular/core';
+import { LanguageService } from '../../../../core/language';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-projects-section',
+  imports: [RouterLink],
+  templateUrl: './projects-section.component.html',
+  styleUrl: './projects-section.component.css',
+})
+export class ProjectsSectionComponent {
+  constructor(public langService: LanguageService) {}
+  get currentLang() {
+    return this.langService.getLang();
+  }
+}
